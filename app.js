77fd2085fd6e1c20471629e90bae8ac844cddca3
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-05-09 17:47:20 +0530

    add

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const mongodbConnection = require("./mongodb");
 const corsOptions = require("./cors");
 const cors = require("cors")
