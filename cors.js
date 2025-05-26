7483bc41b9150a76aa8afc7fdd3cb0ddd7f83eee
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-05-26 06:59:37 +0530

    changed cors coptios

--- cors.js
+++ cors.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 
- const corsOptions = {
+const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (origin && allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("CORS access blocked: Origin not allowed"));
@@ -12,6 +12,5 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
   },
   credentials: true,
 };
-
 
 module.exports = corsOptions;
